21972f4f9018b2e7b5314738c1caa5af046a07c4
matplona <50826369+matplona@users.noreply.github.com>
2022-05-13 15:14:52 +0200

    completed standing drone example

--- extension/examples/hand_driven_drone/standing_drone_variable_height.py
+++ extension/examples/hand_driven_drone/standing_drone_variable_height.py
@@ -20,8 +20,8 @@ DEFAULT_HEIGHT = 0.5
 
 def adjust_height(zrange_state : dict, mc : MotionCommander):
     h = zrange_state['zrange']/1000
-    if isinstance(mc, MockMotionCommander):
-        mc.set_h(h)
+    # if isinstance(mc, MockMotionCommander):
+    #     mc.set_h(h)
     if h < DEFAULT_HEIGHT + threshold:
         mc.start_linear_motion(0,0,ADJUST_VELOCITY, 0) # raise height
     elif h > DEFAULT_HEIGHT - threshold:
@@ -30,24 +30,24 @@ def adjust_height(zrange_state : dict, mc : MotionCommander):
         mc.start_linear_motion(0,0,0,0) # hover fixed
 
 
-class MockMotionCommander:
-    def __init__(self, cf, default_height) -> None:
-        self.__h = default_height
-        return
-    def start_linear_motion(self,vx,vy,vz,vyaw):
-        if vz > 0:
-            print(f'{Fore.GREEN}H={self.__h} ->Raising{Style.RESET_ALL}')
-        elif vz < 0:
-            print(f'{Fore.RED}H={self.__h} ->Lowering{Style.RESET_ALL}')
-        else:
-            print(f'{Fore.BLUE}H={self.__h} ->Hovering{Style.RESET_ALL}')
-    def set_h(self, h):
-        self.__h = h
-    def __enter__(self):
-        print('using MOCK commander')
-        return self
-    def __exit__(self, exc_type, exc_val, exc_t):
-        return
+# class MockMotionCommander:
+#     def __init__(self, cf, default_height) -> None:
+#         self.__h = default_height
+#         return
+#     def start_linear_motion(self,vx,vy,vz,vyaw):
+#         if vz > 0:
+#             print(f'{Fore.GREEN}H={self.__h} ->Raising{Style.RESET_ALL}')
+#         elif vz < 0:
+#             print(f'{Fore.RED}H={self.__h} ->Lowering{Style.RESET_ALL}')
+#         else:
+#             print(f'{Fore.BLUE}H={self.__h} ->Hovering{Style.RESET_ALL}')
+#     def set_h(self, h):
+#         self.__h = h
+#     def __enter__(self):
+#         print('using MOCK commander')
+#         return self
+#     def __exit__(self, exc_type, exc_val, exc_t):
+#         return
 
 if __name__ == '__main__':
     uri = uri_helper.uri_from_env(default='radio://0/80/2M/E7E7E7E706')
@@ -70,7 +70,7 @@ if __name__ == '__main__':
         input("fly..")
         print(f'cf param motion.disable = {ecf.cf.param.get_value("motion.disable")}')
         print(f'cf param motion.disableZrange = {ecf.cf.param.get_value("motion.disableZrange")}')
-        with MockMotionCommander(ecf.cf, default_height=DEFAULT_HEIGHT) as mc:
+        with MotionCommander(ecf.cf, default_height=DEFAULT_HEIGHT) as mc:
             ecf.coordination_manager.observe(
                 observable_name= zranger.observable_name,
                 action= adjust_height,
