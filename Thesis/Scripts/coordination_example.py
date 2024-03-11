951216c1f8dacf6ec2d136cb4422d1b3d45a718a
matplona <50826369+matplona@users.noreply.github.com>
2024-03-11 04:23:03 +0100

    completed chapters 5 and 6

---
+++ Thesis/Scripts/coordination_example.py
@@ -0,0 +1,51 @@
+from cflib.positioning.motion_commander import MotionCommander
+from extension.extended_crazyflie import ExtendedCrazyFlie
+import time
+
+def is_below_safe_limit(state: dict):
+    return state['distance'] < 0.5
+
+def avoid_wall(state: dict, mc: MotionCommander, direction):
+    mc[direction](0.5 - state['distance'])
+
+def add_observables(ecf: ExtendedCrazyFlie, directions):
+    for direction in directions:
+        ecf.coordination_manager.add_observable(
+            f'{direction}_observable', {'distance': 0})
+
+def add_variables(ecf: ExtendedCrazyFlie, directions):
+    for direction in directions:
+        ecf.logging_manager.add_variable('range', direction, 100)
+
+def set_watchers(ecf: ExtendedCrazyFlie, directions):
+    for direction in directions:
+        ecf.logging_manager.set_variable_watcher('range', direction,  
+            lambda distance: ecf.coordination_manager.update_observable_state(
+                f'{direction}_observable', {distance}))
+        
+def observe(ecf: ExtendedCrazyFlie, mc: MotionCommander, directions_and_opposite):
+    for [direction, opposite] in directions_and_opposite:
+        ecf.coordination_manager.observe(
+                observable_names=f'{direction}_observable',
+                condition=is_below_safe_limit,
+                action=avoid_wall,
+                context=[mc, opposite])
+
+if __name__ == '__main__':
+    with ExtendedCrazyFlie('radio://0/80/2M/E7E7E7E7E7') as ecf:
+        with MotionCommander() as mc:
+            # observables setup
+            add_observables(ecf, ['left', 'right', 'front', 'back'])
+
+            # communication setup 
+            add_variables(ecf, ['left', 'right', 'front', 'back'])
+            
+            # watch updates for left range and updates the relative observable
+            set_watchers(ecf, ['left', 'right', 'front', 'back'])
+            
+            # observe the state and act accordingly
+            observe(ecf, mc, 
+                    [['left', 'right'], ['right', 'left'],
+                     ['front', 'back'], ['back', 'front']])
+            
+            time.sleep(100) # wait end of execution
--- Thesis/Thesis.lol
+++ Thesis/Thesis.lol
@@ -1,2 +1,3 @@
 \contentsline {lstlisting}{\numberline {3.1}{\ignorespaces Waypoint navigation implemented using \textit {simple commander}}}{34}{lstlisting.3.1}%
 \contentsline {lstlisting}{\numberline {3.2}{\ignorespaces Waypoint navigation implemented using \textit {motion commander}}}{35}{lstlisting.3.2}%
+\contentsline {lstlisting}{\numberline {6.1}{\ignorespaces Wall avoidance navigation implemented using \textit {Communication and Coordination Frameworks}}}{59}{lstlisting.6.1}%
